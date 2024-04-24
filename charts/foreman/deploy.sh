f7dc7e92de544fcd219fcb1694c449515d48e9e3
Jonathan Gazeley <me@jonathangazeley.com>
2024-04-24 21:53:49 +0100

    Manage rails secret

--- charts/foreman/deploy.sh
+++ charts/foreman/deploy.sh
@@ -2,13 +2,10 @@
 
 k="kubectl"
 set -e
-export RAILS_SECRET=`uuidgen`
 
 $k create namespace foreman
 
 $k config set-context $(kubectl config current-context) --namespace=foreman
-
-$k create secret generic secret-key-base --from-literal=secret-key-base=${RAILS_SECRET}
 
 $k create -f services/rails_svc.yaml
 $k create -f jobs/setup.yaml
