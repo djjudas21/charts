776f06e7d433d2e7116ba866e2b62ae602725428
Jonathan Gazeley <me@jonathangazeley.com>
2024-04-25 17:20:33 +0100

    Run migration job

--- charts/foreman/deploy.sh
+++ charts/foreman/deploy.sh
@@ -8,5 +8,4 @@ $k create namespace foreman
 $k config set-context $(kubectl config current-context) --namespace=foreman
 
 $k create -f services/rails_svc.yaml
-$k create -f jobs/setup.yaml
 $k create -f ingresses/ingress.yaml
