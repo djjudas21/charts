213aef947ca493aecd63d28ae4032f2298f3a22f
Jonathan Gazeley <me@jonathangazeley.com>
2024-04-25 17:03:40 +0100

    Work on deployment templates

--- charts/foreman/deploy.sh
+++ charts/foreman/deploy.sh
@@ -9,7 +9,5 @@ $k config set-context $(kubectl config current-context) --namespace=foreman
 
 $k create -f services/rails_svc.yaml
 $k create -f jobs/setup.yaml
-$k create -f deployments/rails_deploy.yaml
-$k create -f deployments/worker_deploy.yaml
 $k create -f ingresses/ingress.yaml
 $k create -f web-autoscaler.yml
